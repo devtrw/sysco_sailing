79bf8ffc64f0ccd21557a3f8d53e7066b6eacba7
Steven Nance <steven@devtrw.com>
2014-02-13 07:17:50 -0800

    setup initial devise email-only signup flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+
+  def only_if_unconfirmed
+    pending_any_confirmation {yield}
+  end
 end
