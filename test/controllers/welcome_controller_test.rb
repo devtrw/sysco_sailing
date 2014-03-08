9c443fdd020827108ac80eb157a98c5b8c5a2e27
Steven Nance <steven@devtrw.com>
2014-03-08 06:43:43 -0800

    fix tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  test 'should redirect to login page if no user is logged in' do
+    get :index
+    assert_response :redirect
+  end
+
+  test 'should get index for logged in user' do
+    #@user = User.find_by_email 'user1@example.com'
+    @user = users(:two)
+    sign_in @user
     get :index
     assert_response :success
+    assert_template :index
   end
 
 end
